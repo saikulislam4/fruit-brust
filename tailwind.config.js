27fbe1b577800535a01349dd9ca1bf497c3f3de4
saikulislam4 <saikulislam505@gmail.com>
2023-07-25 22:03:03 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+// custom config
+//   tailwind.config = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           clifford: '#da373d',
+//         }
+//       }
+//     }
+//   }
